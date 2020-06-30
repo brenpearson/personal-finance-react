3459e303d32f2a7d2651b6fb9e59721a043c9167
Bren Pearson <bren.pearson0100@gmail.com>
2020-06-30 16:47:13 +1000

    Change for heroku

--- backend/backend.py
+++ backend/backend.py
@@ -1,10 +1,8 @@
 import flask
 from flask import jsonify
-from flask_cors import CORS
 import accounts as Accounts
 
 app = flask.Flask(__name__)
-CORS(app)
 
 @app.route('/accounts/<id>', methods=['GET'])
 def getAccount(id):
